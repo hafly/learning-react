d2addf5bf4736b2b957dc23d3fbee05fbe7d662b
hafly <188150920@qq.com>
2019-06-20 18:52:03 +0800

    组件的组合、嵌套和组件树

--- src/index.js
+++ src/index.js
@@ -1,12 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Title extends Component {
+    render () {
+        return (
+            <h1>React 小书</h1>
+        )
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Header extends Component {
+    render () {
+        return (
+            <div>
+                <Title />
+                <h2>This is Header</h2>
+            </div>
+        )
+    }
+}
+
+class Main extends Component {
+    render () {
+        return (
+            <div>
+                <h2>This is main content</h2>
+            </div>
+        )
+    }
+}
+
+class Footer extends Component {
+    render () {
+        return (
+            <div>
+                <h2>This is footer</h2>
+            </div>
+        )
+    }
+}
+
+class Index extends Component {
+    render () {
+        return (
+            <div>
+                <Header />
+                <Main />
+                <Footer />
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <Index />,
+    document.getElementById('root')
+)
