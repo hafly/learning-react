991f9a568d4e23d117083290c681f20d5a121cab
hafly <188150920@qq.com>
2019-06-21 15:24:21 +0800

    React.js 的 context

--- src/index.js
+++ src/index.js
@@ -1,18 +1,84 @@
 import React,{Component} from 'react'
 import ReactDOM from 'react-dom'
-import wrapWithLoadData from './wrapWithLoadData'
+import PropTypes from 'prop-types';
 
-// 高阶组件其实就是为了组件之间的代码复用，它通过装饰者模式实现
-// 高阶组件内部的包装组件和被包装组件之间通过 props 传递数据。
-class InputWithUserName extends Component {
+// 第一层
+class Index extends Component {
+    static childContextTypes = {
+        themeColor: PropTypes.string
+    }
+
+    constructor () {
+        super()
+        this.state = { themeColor: 'red' }
+    }
+
+    componentWillMount () {
+        this.setState({ themeColor: 'green' })
+    }
+
+    getChildContext () {
+        return { themeColor: this.state.themeColor }
+    }
+
     render () {
-        return <input value={this.props.data} readOnly/>
+        return (
+            <div>
+                <Header />
+                <Main />
+            </div>
+        )
     }
 }
 
-InputWithUserName = wrapWithLoadData(InputWithUserName, 'username')
+// 第二层
+class Header extends Component {
+    render () {
+        return (
+            <div>
+                <h2>This is header</h2>
+                <Title />
+            </div>
+        )
+    }
+}
+
+class Main extends Component {
+    render () {
+        return (
+            <div>
+                <h2>This is main</h2>
+                <Content />
+            </div>
+        )
+    }
+}
+
+// 第三层
+class Title extends Component {
+    // 任意深度的子组件都可以通过 contextTypes 来声明你想要的 context 里面的哪些状态，然后可以通过 this.context 访问到那些状态
+    static contextTypes = {
+        themeColor: PropTypes.string
+    }
+
+    render () {
+        return (
+            <h1 style={{ color: this.context.themeColor }}>React.js 小书标题</h1>
+        )
+    }
+}
+
+class Content extends Component {
+    render () {
+        return (
+            <div>
+                <h2>React.js 小书内容</h2>
+            </div>
+        )
+    }
+}
 
 ReactDOM.render(
-    <InputWithUserName/>,
+    <Index />,
     document.getElementById('root')
 )
