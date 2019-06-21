0d79d35d685953c0de5331e22091cbc957f9531b
hafly <188150920@qq.com>
2019-06-21 11:10:04 +0800

    挂载阶段的组件生命周期（一）

--- src/index.js
+++ src/index.js
@@ -1,9 +1,61 @@
-import React from 'react'
+import React,{Component} from 'react'
 import ReactDOM from 'react-dom'
-import CommentApp from './CommentApp'
-import './index.css'
+
+class Header extends Component {
+    constructor () {
+        super()
+        console.log('construct')
+    }
+
+    componentWillMount () {
+        console.log('component will mount')
+    }
+
+    componentDidMount () {
+        console.log('component did mount')
+    }
+
+    componentWillUnmount(){
+        console.log('component will unmount')
+    }
+
+    render () {
+        console.log('render')
+        return (
+            <div>
+                <h1 className='title'>React 小书</h1>
+            </div>
+        )
+    }
+}
+
+class Index extends Component {
+    constructor() {
+        super()
+        this.state = {
+            isShowHeader: true
+        }
+    }
+
+    handleShowOrHide () {
+        this.setState({
+            isShowHeader: !this.state.isShowHeader
+        })
+    }
+
+    render () {
+        return (
+            <div>
+                {this.state.isShowHeader ? <Header /> : null}
+                <button onClick={this.handleShowOrHide.bind(this)}>
+                    显示或者隐藏标题
+                </button>
+            </div>
+        )
+    }
+}
 
 ReactDOM.render(
-    <CommentApp />,
+    <Index />,
     document.getElementById('root')
 )
