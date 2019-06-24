259dd547570a107707aec330441516d8785c0c2c
hafly <188150920@qq.com>
2019-06-24 09:09:37 +0800

    实战分析：评论功能（九）

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react'
-import ReactDOM from 'react-dom';
-import Index from './React-redux/Index'
+import ReactDOM from 'react-dom'
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+import CommentApp from './containers/CommentApp'
+import commentsReducer from './reducers/comments'
+import './index.css'
+
+const store = createStore(commentsReducer)
 
 ReactDOM.render(
-    <Index />,
+    <Provider store={store}>
+        <CommentApp/>
+    </Provider>,
     document.getElementById('root')
-)
+);
